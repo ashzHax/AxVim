050d80e69a42094d620186ee6f965599d14fae83
ashzHax <ashkim2001@gmail.com>
2021-05-09 00:29:33 +0900

    code prefix cleanup

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@ SYS_VIM_PATH="${SYS_HOME_PATH}/.vim"
 # main
 echo "[alert] starting installation" 
 
-echo "[command] cp $AXV_VIMRC_PATH $SYS_VIMRC_PATH"
+echo "[cmmnd] cp $AXV_VIMRC_PATH $SYS_VIMRC_PATH"
 cp $AXV_VIMRC_PATH $SYS_VIMRC_PATH
 
-echo "[command] mkdir -p $SYS_VIM_PATH"
+echo "[cmmnd] mkdir -p $SYS_VIM_PATH"
 mkdir -p $SYS_VIM_PATH
 
-echo "[command] cp -r $AXV_VIM_DIR_PATH $SYS_VIM_PATH"
+echo "[cmmnd] cp -r $AXV_VIM_DIR_PATH $SYS_VIM_PATH"
 cp -r $AXV_VIM_DIR_PATH $SYS_VIM_PATH
 
 echo "[alert] finished installation"
