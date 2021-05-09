37147f54e014edf2aeb3675fc9c32972ea777132
ashzHax <ashkim2001@gmail.com>
2021-05-09 05:53:21 -0700

    install script edit

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
 # By : ashz
-# Ex : Install script for AxVim.
+# Ex : install script for AxVim
 
 AXV_VIMRC_PATH=./vimrc
 AXV_VIM_DIR_PATH=./vim
@@ -10,16 +9,16 @@ SYS_VIMRC_PATH="${SYS_HOME_PATH}/.vimrc"
 SYS_VIM_PATH="${SYS_HOME_PATH}/.vim"
 
 # main
-echo "[alert] starting installation" 
+echo "[alert] starting AxVim installation" 
 
-echo "[command] cp $AXV_VIMRC_PATH $SYS_VIMRC_PATH"
+echo "[cmmnd] cp $AXV_VIMRC_PATH $SYS_VIMRC_PATH"
 cp $AXV_VIMRC_PATH $SYS_VIMRC_PATH
 
-echo "[command] mkdir -p $SYS_VIM_PATH"
+echo "[cmmnd] mkdir -p $SYS_VIM_PATH"
 mkdir -p $SYS_VIM_PATH
 
-echo "[command] cp -r $AXV_VIM_DIR_PATH $SYS_VIM_PATH"
-cp -r $AXV_VIM_DIR_PATH $SYS_VIM_PATH
+echo "[cmmnd] cp -r ${AXV_VIM_DIR_PATH}/* ${SYS_VIM_PATH}/"
+cp -r ${AXV_VIM_DIR_PATH}/* ${SYS_VIM_PATH}/
 
-echo "[alert] finished installation"
+echo "[alert] completed"
 
